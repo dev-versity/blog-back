43bc4bf68c381311910c0e11ffeb42eeb880dbce
beuleu <billyklotz789@gmail.com>
2023-02-08 15:22:16 +0100

    new method get one article

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,27 +1,27 @@
-const Article = require("../models/Article");
+const Article       = require("../models/Article");
 const articleRouter = require("express").Router();
 
 articleRouter.get("/all", (req, res) => {
   Article.find({}, (err, articles) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(articles);
-    }
+    if ( err ) res.send(err);
+    res.send(articles);
   })
 });
+
+articleRouter.get("/:id", (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    if ( err ) res.send(err)
+    res.send(article)
+  })
+})
 
 articleRouter.post("/create", (req, res) => {
   let newArticle = new Article(req.body);
   newArticle.save((error, article) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(article);
-    }
+    if ( error ) res.send(error)
+    res.send(article);
   });
 })
-
 
 
 module.exports = articleRouter;
