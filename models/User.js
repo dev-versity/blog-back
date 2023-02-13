61fe0f5fda6f7b4502fea1387f15d5068b300582
Raftouch <rafanelkin@gmail.com>
2023-02-13 12:32:59 +0100

    :sparkles: adding password encrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
     email: String,
@@ -8,6 +9,12 @@ const userSchema = new Schema({
         min: 6
     }
   });
+
+userSchema.methods.encryptPassword = async password => {
+    const salt = await bcrypt.genSalt(5)
+    const hash = await bcrypt.hash(password, salt)
+    return hash
+}
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,9 +5,11 @@ userRouter.get('/signup', (req, res) => {
     res.render('signup')
 })
 
-userRouter.post('/signup', (req, res) => {
-    let newUser = new User(req.body);
-  newUser.save((error, user) => {
+userRouter.post('/signup', async (req, res) => {
+    let newUser = new User();
+    newUser.email = req.body.email;
+    newUser.password = await newUser.encryptPassword(req.body.password);
+    newUser.save((error, user) => {
     if ( error ) res.send(error)
     res.send(user);
   });
